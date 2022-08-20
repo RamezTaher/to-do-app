b6a6a85c1113be0dfd27120ad22043c4c03cafb4
Ramez Taher <Rameztaher2002@gmail.com>
2022-08-20 17:21:22 +0100

    events for keyboard & inputs

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-const clearBtn = document.querySelector(".clear-tasks")
-const card = document.querySelector(".card")
-const heading = document.querySelector("h5")
+const form = document.querySelector("form")
+const taskInput = document.getElementById("task")
 
-// Click
-// clearBtn.addEventListener("click", runEvent)
-// Doubleclick
-// clearBtn.addEventListener("dblclick", runEvent)
-// MouseDown
-// clearBtn.addEventListener("mousedown", runEvent)
-// MouseUp
-// clearBtn.addEventListener("mouseup", runEvent)
-// MouseEnter
-// card.addEventListener("mouseenter", runEvent)
-// Mouse Leave
-// card.addEventListener("mouseleave", runEvent)
-// Mouse Move
-card.addEventListener("mousemove", runEvent)
+// Clear Input
+taskInput.value = ""
 
-// Event Handler
+form.addEventListener("submit", runEvent)
+
+taskInput.addEventListener("keydown", runEvent)
+// keydown
+// keyup
+// keypress
+
+// focus
+// blur
+// cut
+// paste
+// input
+
 function runEvent(e) {
-  e.preventDefault()
-  console.log(`rgb(${e.offSetX},${e.offSetY},50)`)
-  document.body.style.backgroundColor = `rgb(${e.offsetX},${e.offsetY},50)`
+  console.log(`event type: ${e.type}`)
+  console.log(e.target.value)
+  // Get input value
+  console.log(taskInput.value)
 }
