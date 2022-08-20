7c069c7abd05e2485094d026e5f5fd379a086d44
Ramez Taher <Rameztaher2002@gmail.com>
2022-08-20 17:11:40 +0100

    mouse Events

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-document.querySelector(".clear-tasks").addEventListener("click", onClick)
+const clearBtn = document.querySelector(".clear-tasks")
+const card = document.querySelector(".card")
+const heading = document.querySelector("h5")
 
-function onClick(e) {
-  // console.log("Button Clicked")
+// Click
+// clearBtn.addEventListener("click", runEvent)
+// Doubleclick
+// clearBtn.addEventListener("dblclick", runEvent)
+// MouseDown
+// clearBtn.addEventListener("mousedown", runEvent)
+// MouseUp
+// clearBtn.addEventListener("mouseup", runEvent)
+// MouseEnter
+// card.addEventListener("mouseenter", runEvent)
+// Mouse Leave
+// card.addEventListener("mouseleave", runEvent)
+// Mouse Move
+card.addEventListener("mousemove", runEvent)
 
-  let val
-  val = e.target
-  val = e.target.id
-  val = e.target.className
-  val = e.target.classList
-
-  console.log(val)
+// Event Handler
+function runEvent(e) {
+  e.preventDefault()
+  console.log(`rgb(${e.offSetX},${e.offSetY},50)`)
+  document.body.style.backgroundColor = `rgb(${e.offsetX},${e.offsetY},50)`
 }
